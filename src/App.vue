6374aa0f66df0caa933feeeca4cf1feb7dac2d4e
Evgeny Kuzmin <skchel174@gmail.com>
2026-03-26 23:29:11 +0500

    create HabitItem component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue';
-
-interface Habit {
-  id: number;
-  title: string;
-  doneToday: boolean;
-}
+import HabitItem from '@/components/HabitItem.vue';
+import type { Habit } from '@/types';
 
 const habits = ref<Habit[]>([
   { id: 1, title: 'Drink water', doneToday: false },
@@ -50,35 +46,12 @@ const toggleHabit = (id: number) => {
       </div>
 
       <div class="flex flex-col gap-2">
-        <div
+        <HabitItem
           v-for="habit in habits"
           :key="habit.id"
-          class="p-4 flex items-center gap-4 bg-white border border-gray-200 shadow-2xs rounded-md"
-        >
-          <div class="flex-1">
-            <h3
-              class="font-semibold text-gray-800"
-              :class="[habit.doneToday ? 'line-through' : 'no-underline']"
-            >
-              {{ habit.title }}
-            </h3>
-            <p class="mt-1 text-xs font-medium uppercase text-gray-500">
-              {{ habit.doneToday ? 'Done' : 'Not done' }}
-            </p>
-          </div>
-          <button
-            type="button"
-            class="py-2 px-4 text-sm font-medium rounded-md border border-transparent disabled:opacity-50 disabled:pointer-events-none cursor-pointer"
-            :class="
-              habit.doneToday
-                ? 'bg-neutral-200 text-neutral-900 hover:bg-neutral-300'
-                : 'bg-pink-200 text-pink-900 hover:bg-pink-300'
-            "
-            @click="toggleHabit(habit.id)"
-          >
-            {{ habit.doneToday ? 'Reset' : 'Complete' }}
-          </button>
-        </div>
+          :habit="habit"
+          @toggle="toggleHabit"
+        />
       </div>
     </main>
   </div>
---
+++ src/components/HabitItem.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import type { Habit } from '@/types';
+
+const props = defineProps<{ habit: Habit }>();
+
+const emit = defineEmits<{
+  (e: 'toggle', id: number): void;
+}>();
+
+const toggleHabit = (id: number) => {
+  emit('toggle', id);
+}
+</script>
+
+<template>
+  <div
+    class="p-4 flex items-center gap-4 bg-white border border-gray-200 shadow-2xs rounded-md"
+  >
+    <div class="flex-1">
+      <h3
+        class="font-semibold text-gray-800"
+        :class="[habit.doneToday ? 'line-through' : 'no-underline']"
+      >
+        {{ habit.title }}
+      </h3>
+      <p class="mt-1 text-xs font-medium uppercase text-gray-500">
+        {{ habit.doneToday ? 'Done' : 'Not done' }}
+      </p>
+    </div>
+    <button
+      type="button"
+      class="py-2 px-4 text-sm font-medium rounded-md border border-transparent disabled:opacity-50 disabled:pointer-events-none cursor-pointer"
+      :class="
+        habit.doneToday
+          ? 'bg-neutral-200 text-neutral-900 hover:bg-neutral-300'
+          : 'bg-pink-200 text-pink-900 hover:bg-pink-300'
+      "
+      @click="toggleHabit(habit.id)"
+    >
+      {{ habit.doneToday ? 'Undo' : 'Complete' }}
+    </button>
+  </div>
+</template>
